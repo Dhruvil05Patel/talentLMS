ec621a32eb5938ce1bf894c3a4ff3c3fb6e8ccd3
Dhruvil05Patel <dhruvil1405patel@gmail.com>
2026-07-11 19:04:00 +0530

    Bug Fixes in scroll behaviours

--- talent_lms/app/layout.tsx
+++ talent_lms/app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       {/* 
         By hard-mapping your local font families right here on the wrapper class list, 
         your custom component styles (like fontFamily: "Hornbill") can instantly read the variables!
